7d03917f094b4d18803048cfd6d50d1efee291e5
Cristina <200101@iessierradeguara.com>
2021-10-25 18:55:43 +0200

    Modificaciones

--- CalculadoraPrimos/app/src/main/java/com/example/calculadoraprimos/MainActivity.java
+++ CalculadoraPrimos/app/src/main/java/com/example/calculadoraprimos/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             num = lista.get(lista.size()-1)+1;
-            for (int i = 2; i <= num / 2; i++) {
+            for (int i = 2; i <= (int)Math.sqrt(num); i++) {
                 if (num % i == 0) {
                     esPrimo = false;
                     break;
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             }
             num++;
             esPrimo=true;
-            for (int i = 2; i <= num/2; i++) {
+            for (int i = 2; i <= (int)Math.sqrt(num); i++) {
                 if(num % i == 0) {
                     esPrimo = false;
                     break;
