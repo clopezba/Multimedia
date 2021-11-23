4d4987a50161cf18ab8b7c6ca79750d9ac358925
Cristina <200101@iessierradeguara.com>
2021-11-23 18:22:04 +0100

    fin p2

--- Practica2/app/src/main/java/com/example/practica2/MainActivity.java
+++ Practica2/app/src/main/java/com/example/practica2/MainActivity.java
@@ -1,31 +1,23 @@
+/**
+ * Aplicación juego BuscAliens, similar al buscaminas. Encontrar en una matriz todos los aliens escondidos sin equivocarse.
+ * @author Cristina López
+ * @version 1.0
+ */
 package com.example.practica2;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -34,26 +26,41 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
     LinearLayout ll, fil;
     Button btn;
-    int filas = 8, columnas = 8, bombas = 10;
-    int contBombas = bombas;
+    int filas, columnas, aliens, contAliens;
+    int nivel;  //Nivel: 0 = Principiante, 1 = Amateur, 2 = Avanzado
 
-    //Nivel: 0 = Principiante, 1 = Amateur, 2 = Avanzado
-    int nivel = 0;
+    /**
+     * Método al que se llama cuando se inicia la pantalla (Activity).
+     * En este método se pone la mayoría de los elementos para la inicialización (funciones, varaibles, etc.).
+     * @param savedInstanceState Permite volver a un estado anterior de la activiad cuando se vuelve a reiniciar. Admite valor nulo.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        crearTabla(filas,columnas);
+        //Inicia por defecto en el nivel principiante (0).
+        seleccionarDificultad(nivel);
 
     }
 
     //--------------- MENÚ -------------------
+
+    /**
+     * Método para crear un menú de opciones y añadirle los ítems que contendrá.
+     * @param menu El menú de opciones donde se colocarán los items.
+     * @return Valor  de tipo booleano: 'True' para que se muestre el menú, 'False' para que no se muestre.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
+
+    /**
+     * Método al que se llama cuando se selecciona un ítem del menú
+     * @param item El ítem que ha sido seleccionado. No puede ser nulo.
+     * @return valor de tipo boolean
+     */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
@@ -64,16 +71,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.nuevo:
                 switch (nivel){
                     case 0:
-                        setContentView(R.layout.activity_main);
-                        crearTabla(filas,columnas);
+                        seleccionarDificultad(nivel);
                         break;
                     case 1:
-                        setContentView(R.layout.activity_main);
-                        crearTabla(filas,columnas);
+                        seleccionarDificultad(nivel);
                         break;
                     case 2:
-                        setContentView(R.layout.activity_main);
-                        crearTabla(filas,columnas);
+                        seleccionarDificultad(nivel);
                         break;
                 }
                 return true;
@@ -84,6 +88,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Método para crear un cuadro de diálogo que incluye las instrucciones del juego.
+     * Texto con un botón para cerrarlo.
+     * @return Devuelve el cuadro de diálogo para que aparezca por pantalla cuando se le llame.
+     */
     public Dialog crearDialogoInstrucciones(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(R.string.instrucciones)
@@ -91,11 +100,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
+                //Se cierra el cuadro de diálogo
             }
         });
         return builder.create();
     }
+    /**
+     * Método para crear un cuadro de diálogo que permite la configuración del juego en tres niveles.
+     * Consta de tres RadioButtons con los que se selecciona el nivel del juego. Cada nivel aplica una configuración diferente.
+     * @return Devuelve el cuadro de diálogo para que aparezca por pantalla cuando se le llame.
+     */
     public Dialog crearDialogoConfig(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.configTitulo)
@@ -104,32 +118,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void onClick(DialogInterface dialogInt, int item) {
                         switch(item){
                             case 0:
-                                nivel = 0;
-                                filas = 8;
-                                columnas = 8;
-                                bombas = 10;
-                                contBombas = bombas;
-                                setContentView(R.layout.activity_main);
-                                crearTabla(filas,columnas);
-
+                                seleccionarDificultad(0);
                                 break;
                             case 1:
-                                nivel = 1;
-                                filas = 12;
-                                columnas = 12;
-                                bombas = 30;
-                                contBombas = bombas;
-                                setContentView(R.layout.activity_main);
-                                crearTabla(filas,columnas);
+                                seleccionarDificultad(1);
                                 break;
                             case 2:
-                                nivel = 2;
-                                filas = 16;
-                                columnas = 16;
-                                bombas = 60;
-                                contBombas = bombas;
-                                setContentView(R.layout.activity_main);
-                                crearTabla(filas,columnas);
+                                seleccionarDificultad(2);
                                 break;
                         }
                     }
@@ -137,43 +132,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         builder.setPositiveButton(R.string.volver, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
+                //Se cierra el cuadro de diálogo
             }
         });
-
         return builder.create();
     }
 
     /**
-     * Crea la matriz sobre la que se añadirán los botones. A éstos se le añade un número aleatorio único que determinará
-     * si hay bomba o no.
-     * @param filas
-     * @param columnas
+     * Método para establecer los valores de configuración del juego según el nivel de dificultad seleccionado.
+     * @param dificultad Recibe un valor correspondiente a la dificultad seleccionada.
+     */
+    public void seleccionarDificultad(int dificultad){
+        switch(dificultad){
+            case 0:
+                nivel = 0;
+                filas = 8;
+                columnas = 8;
+                aliens = 10;
+                contAliens = aliens;
+                setContentView(R.layout.activity_main); //Reinicio la vista para que no sume
+                crearTabla(filas,columnas);
+
+                break;
+            case 1:
+                nivel = 1;
+                filas = 12;
+                columnas = 12;
+                aliens = 30;
+                contAliens = aliens;
+                setContentView(R.layout.activity_main);
+                crearTabla(filas,columnas);
+                break;
+            case 2:
+                nivel = 2;
+                filas = 16;
+                columnas = 16;
+                aliens = 60;
+                contAliens = aliens;
+                setContentView(R.layout.activity_main);
+                crearTabla(filas,columnas);
+                break;
+        }
+    }
+
+    //---------------- CREAR Y RELLENAR MATIZ ---------------
+
+    /**
+     * Crea la matriz sobre la que se añadirán los botones. A estos se le añade un número aleatorio único que determinará
+     * si hay alien o no.
+     * @param filas Filas que tendrá la matriz
+     * @param columnas Columnas que tendrá la matriz
      */
     public void crearTabla(int filas, int columnas){
         ll = (LinearLayout) findViewById(R.id.linear);
         ll.setBackgroundColor(getColor(R.color.white));
+
+        //Parámetros comunes en LinearLayout fila y botones
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
 
         ArrayList<Integer> listaNumeros = numAleatorios(filas*columnas);
         int cont = 0;
 
+        //Creación de filas con LinearLayout
         for (int i = 0; i < filas; i++) {
             fil = new LinearLayout(this);
             fil.setLayoutParams(lp);
             fil.setOrientation(LinearLayout.HORIZONTAL);
-            fil.setGravity(Gravity.CENTER);
             fil.setBackgroundColor(getColor(R.color.black));
 
-
+            //Creación de columnas con botones
             for (int j = 0; j < columnas; j++) {
-                Log.d("consola", listaNumeros.get(cont).toString());
                 btn = new Button(this);
                 btn.setBackground(getDrawable(R.drawable.border));
-                btn.setId(cont);
                 btn.setLayoutParams(lp);
-                btn.setTag(listaNumeros.get(cont));
+                btn.setId(cont); //Id único autoincrementable
+                btn.setTag(listaNumeros.get(cont)); //Número único aleatorio, saber si hay alien o no
                 if(filas == 16)
                     btn.setTextSize(11);
                 else
@@ -189,8 +223,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /**
      * Genera un ArrayList con tantos números aleatorios como celdas haya en la matriz.
-     * @param celdas
-     * @return
+     * @param celdas Número de celdas de la matriz
+     * @return Lista de números (de 0 a celdas) aleatorizada
      */
     public ArrayList<Integer> numAleatorios(int celdas){
         ArrayList<Integer> list = new ArrayList<Integer>();
@@ -202,10 +236,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     * Crea una matriz del tamaño de la cuadrícula y resetea los valores a 0
-      * @param filas
-     * @param columnas
-     * @return
+     * Crea una matriz del tamaño de la cuadrícula y la inicia con todos sus valores a 0
+      * @param filas Número de filas de la matriz
+     * @param columnas Número de columnas de la matriz
+     * @return Devuelve una matriz de tamaño similar al de la vista creada
      */
     public int[][] crearMatriz(int filas, int columnas){
         int matriz[][] = new int [filas][columnas];
@@ -218,17 +252,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     * A partir de la matriz generada, recorre los elementos que contiene el layout. Si el elemento tiene una bomba (según el valor de la 'Tag')
+     * A partir de la matriz generada, recorre los elementos que contiene el layout. Si el elemento tiene un alien (según el valor de la 'Tag')
      * pone un número negativo en su respectiva posición de la matriz. Recorre los elementos adyacentes y aumenta su valor coreespondiente que
-     * será la pista que indica las bombas que hay a su alrededor.
-     * @param filas
-     * @param columnas
-     * @param bombas
-     * @return
+     * será la pista que indica los aliens que hay a su alrededor.
+     * @param filas Número de filas de la matriz
+     * @param columnas Número de columnas de la matriz
+     * @param aliens Número de aliens que pondremos
+     * @return Lista (ArrayList) con las pistas que ayudan a descubrir los aliens
      */
-    public ArrayList<Integer> recorrer(int filas, int columnas, int bombas){
+    public ArrayList<Integer> recorrer(int filas, int columnas, int aliens){
         int matriz[][] = crearMatriz(filas, columnas);
-        ArrayList<Integer> pistasBombas = new ArrayList<Integer>();
+        ArrayList<Integer> pistasAliens = new ArrayList<Integer>();
         LinearLayout lin;
         View v, v2;
         for (int i = 0; i < ll.getChildCount(); i++) {
@@ -237,7 +271,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             for (int j = 0; j < lin.getChildCount(); j++) {
                 v2 = lin.getChildAt(j);
-                if ((Integer)v2.getTag() < bombas) {
+                if ((Integer)v2.getTag() < aliens) {
                     matriz[i][j] = -10;
                     if (i == 0 && j==0) { //Arriba - izquierda
                        matriz[i][j+1] += 1;
@@ -300,19 +334,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         }
+        //Guarda todos los nuevos valores de la matriz en una lista (ArrayList)
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                pistasBombas.add(matriz[i][j]);
+                pistasAliens.add(matriz[i][j]);
             }
         }
-        return pistasBombas;
+        return pistasAliens;
     }
+
+    //------------------- MÉTODOS PULSACIONES -----------------------
+
+    /**
+     * Define la acción o acciones que se llevarán a cabo cuando se pulse sobre una vista de la aplicación (click corto)
+     * @param view Elemento o vista que ha sido pulsado
+     */
     @Override
     public void onClick(View view) {
-        ArrayList<Integer> valores = recorrer(filas, columnas, bombas);
+        ArrayList<Integer> valores = recorrer(filas, columnas, aliens);
+        //Guarda en un entero el valor del tag del botón (indica si hay alien o no)
         int numCelda = Integer.parseInt(((Button)view).getTag().toString());
 
-        if(numCelda < bombas){
+        //Si es un alien
+        if(numCelda < aliens){
             Button b = (Button) view;
             b.setBackground(getDrawable(R.drawable.mal));
             deshabilitarCeldas();
@@ -327,33 +371,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-
+    /**
+     * Define la acción o acciones que se llevarán a cabo cuando se pulse con un click largo sobre una vista de la aplicación
+     * @param view Elemento o vista que ha sido pulsado
+     */
     @Override
     public boolean onLongClick(View view) {
-        ArrayList<Integer> valores = recorrer(filas, columnas, bombas);
+        ArrayList<Integer> valores = recorrer(filas, columnas, aliens);
         int numCelda = Integer.parseInt(((Button)view).getTag().toString());
 
-        if(numCelda < bombas){
+        if(numCelda < aliens){
             Button b = (Button) view;
             b.setBackground(getDrawable(R.drawable.bien));
             b.setOnLongClickListener(null);
             b.setOnClickListener(null);
-            contBombas--;
-            if(contBombas > 0) {
-                String texto = getString(R.string.longClick_bien1) + " "+ contBombas + " " + getString(R.string.longClick_bien2);
+            contAliens--;
+            //Si no es el último alien
+            if(contAliens > 0) {
+                String texto = getString(R.string.longClick_bien1) + " "+ contAliens + " " + getString(R.string.longClick_bien2);
                 Toast.makeText(this, texto, Toast.LENGTH_LONG).show();
                 b.setEnabled(false);
             }
-            else
+            //Si es el último alien
+            else {
                 Toast.makeText(this, getString(R.string.longClick_ganado), Toast.LENGTH_LONG).show();
+                deshabilitarCeldas();
+            }
         }
+        //Si no escondía un alien
         else{
             Toast.makeText(this, getString(R.string.longClick_mal), Toast.LENGTH_LONG).show();
             deshabilitarCeldas();
         }
-
         return false;
     }
+
+    /**
+     * Recorre todas las celdas de la matriz y las deshabilita para que no puedan ser pulsadas.
+     */
     public void deshabilitarCeldas() {
         LinearLayout lin;
         View v, v2;
@@ -367,5 +422,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
-
 }
